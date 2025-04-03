55ba336700cdf139712304109b2ea8a0f0a7c0f7
Kiara Johnson <johns.kia35@gmail.com>
2025-04-03 16:22:36 -0600

    add react

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+import { createRoot } from 'react-dom/client'
 //login auth
 
 //login upon successful auth goes to landing.html
 
-//landing.html log button goes to log view and auto adds time and date
+//landing.html log button goes to log view and auto adds time and date and takes you to the symptom log area.
 
